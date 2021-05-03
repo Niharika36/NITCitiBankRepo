501872ebd8395ebb80f826c5cc26d070d6e7a64f
Dev1 <Dev1@gmail.com>
2021-05-03 18:06:56 +0530

    Written 'Test.java'by Dev1

--- DemoGitProj1/src/com/nit/beans/Utility.java
+++ DemoGitProj1/src/com/nit/beans/Utility.java
@@ -6,8 +6,4 @@ public class Utility {
 	public Date getByDate() {
 		return new Date();
 	}
-	public static void main(String[] args) {
-		System.out.println("DATE:"+new Utility().getByDate());
-	}
-
 }
